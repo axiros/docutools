fcca96dc6ac127c576f3ff1895ac8f1cce1e6c22
Gunther Klessinger <g_kl_es_sing_er@gmx.de>
2021-09-06 18:35:38 +0200

    tests: travis

--- tests/test_lp.py
+++ tests/test_lp.py
@@ -79,6 +79,7 @@ def check_lines_in(res, *blocks):
     Plus "the dot, which we'll remove some day ..."
     """
     res = [l.rstrip() for l in res.splitlines()]
+    breakpoint()  # FIXME BREAKPOINT
     print('check_lines')
     print(res)
     print('????????')
@@ -86,7 +87,7 @@ def check_lines_in(res, *blocks):
         for l in b.splitlines():
 
             if l.strip():
-                kk = l.strip()
+                kk = l.rstrip()
                 print('kk')
                 print(kk)
                 assert kk in res
