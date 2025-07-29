6ca98f3ccb25e095e75aeb60e87e6a5b18917b5a
Gunther Klessinger <g_kl_es_sing_er@gmx.de>
2025-07-29 17:19:22 +0200

    fmt

--- src/lcdoc/mkdocs/lp/plugs/python/pyplugs/__init__.py
+++ src/lcdoc/mkdocs/lp/plugs/python/pyplugs/__init__.py
@@ -1,13 +1,12 @@
-from . import (
-    call_flow_logging,
-    comments,
-    convert,
-    cov_report,
-    data_table,
-    diag_diagram,
-    git_changelog,
-    lprunner,
-    mpl_pyplot,
-    project_dependencies,
-    screenshot,
-)
+breakpoint()  # FIXME BREAKPOINT
+from . import call_flow_logging
+from . import comments
+from . import convert
+from . import cov_report
+from . import data_table
+from . import diag_diagram
+from . import git_changelog
+from . import lprunner
+from . import mpl_pyplot
+from . import project_dependencies
+from . import screenshot
